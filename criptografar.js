b1352538877b7aec869a64059a2b39c05d2a3bfb
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-01-02 11:22:43 -0300

    Faltando apenas responsividade

--- criptografar.js
+++ criptografar.js
@@ -1,6 +1,21 @@
-
 var mensagemUsuario = document.querySelector("#mensagem-usuario");
-var mensagemCriptografada = document.querySelector("#texto-criptografado");
+var mensagemCriptografada = document.querySelector(".texto-criptografado");
+var meninoDaLupa = document.querySelector(".resposta");
+var mensagem1 =  document.querySelector (".mensagem1");
+var mensagem2 = document.querySelector (".mensagem2");
+var aparenciaInicial = document.querySelector (".texto-sobrepor");
+
+function remover (){
+
+    meninoDaLupa.classList.remove("resposta");
+    meninoDaLupa.classList.add("resposta2");
+    mensagem1.classList.remove("mensagem1");
+    mensagem1.classList.add("notmensage");
+    mensagem2.classList.remove("mensagem2");
+    mensagem2.classList.add("notmensage2");
+    aparenciaInicial.classList.remove("texto-sobrepor");
+    aparenciaInicial.classList.add("texto-sobrepor-none");
+}
 
 
 function criptografar() {
@@ -11,34 +26,49 @@ function criptografar() {
         .replace(/i/g, "imes")
         .replace(/a/g, "ai")
         .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+        .replace(/u/g, "ufat")
+        .replace(/E/g, "enter")
+        .replace(/I/g, "imes")
+        .replace(/A/g, "ai")
+        .replace(/O/g, "ober")
+        .replace(/U/g, "ufat");
 
-        /*    Podemos esrever o replace " Substituir " também desta forma.
 
-        .replaceAll("e","enter")
-
-        */ 
-
-    mensagemCriptografada.value = criptografado
+    /*    Podemos esrever o replace " Substituir " também desta forma.
+    .replaceAll("e","enter")
+    */
+    remover ();
+    mensagemCriptografada.value = criptografado;
 
 }
 
-function decriptografar() {
+    function decriptografar() {
 
-    var mensagem = mensagemUsuario.value;
+        var mensagem = mensagemUsuario.value;
 
-    var descriptografa = mensagem.replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
+        var descriptografa = mensagem.replace(/enter/g, "e")
+            .replace(/imes/g, "i")
+            .replace(/ai/g, "a")
+            .replace(/ober/g, "o")
+            .replace(/ufat/g, "u")
+            .replace(/ENTER/g, "e")
+            .replace(/IMES/g, "i")
+            .replace(/AI/g, "a")
+            .replace(/OBER/g, "o")
+            .replace(/Ufat/g, "u")
+            .replace(/Enter/g, "e")
+            .replace(/Imes/g, "i")
+            .replace(/Ai/g, "a")
+            .replace(/Ober/g, "o")
+            .replace(/Ufat/g, "u");
 
         mensagemCriptografada.value = descriptografa;
 
-}
+    }
 
-function copiar() {
+    function copiarMensaguemCriptografada() {
 
+        document.execCommand("copy");
 
-
-}
+    }
+    
